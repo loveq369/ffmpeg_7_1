0f33fc0b786c95b9dd55e43c9f9e3bdddf320695
loveq369 <longhaiqiang@me.com>
2024-12-23 20:06:17 +0800

    build it

---
+++ build/libavfilter/vf_yadif_videotoolbox.d
@@ -0,0 +1,12 @@
+libavfilter/vf_yadif_videotoolbox.o: libavfilter/vf_yadif_videotoolbox.m \
+  libavfilter/filters.h libavfilter/avfilter.h libavutil/attributes.h \
+  libavutil/avutil.h libavutil/common.h libavutil/error.h \
+  libavutil/macros.h libavutil/avconfig.h libavutil/version.h config.h \
+  libavutil/intmath.h libavutil/internal.h libavutil/libm.h \
+  libavutil/intfloat.h libavutil/mathematics.h libavutil/rational.h \
+  libavutil/log.h libavutil/pixfmt.h libavutil/buffer.h libavutil/dict.h \
+  libavutil/frame.h libavutil/channel_layout.h libavutil/samplefmt.h \
+  libavfilter/version_major.h libavfilter/metal/utils.h \
+  libavfilter/yadif.h libavutil/opt.h libavutil/pixdesc.h \
+  libavfilter/ccfifo.h libavutil/avassert.h libavutil/hwcontext.h \
+  libavutil/hwcontext_videotoolbox.h libavutil/objc.h
